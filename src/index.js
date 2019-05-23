f32faf612c6f8e49f479118fec2005b49c236d56
Manoel Carvalho <manoelmecat@gmail.com>
2019-05-23 16:39:13 -0300

    Styled Components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { StatusBar } from "react-native";
 
-import '~/config/ReactotronConfig';
+import "~/config/ReactotronConfig";
 
-import Routes from '~/routes';
+import Routes from "~/routes";
 
-const App = () => <Routes />;
+const App = () => (
+  <>
+    <Routes />
+  </>
+);
 
 export default App;
